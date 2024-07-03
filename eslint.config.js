950dc3f5acea3aaa69ff9d7149d566be36628ec9
rayc <cr_fun@qq.com>
2024-07-04 01:03:59 +0800

    Initialize the project

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import pluginVue from 'eslint-plugin-vue'
+import parserVue from 'vue-eslint-parser'
+import eslintConfigPrettier from 'eslint-config-prettier'
+
+export default [
+  { ignores: ['**/node_modules', '**/dist/'] },
+  { files: ['**/*.{js,mjs,cjs,ts,vue}'] },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  {
+    languageOptions: {
+      parser: parserVue,
+      parserOptions: { parser: tseslint.parser },
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...pluginVue.configs['flat/essential'],
+  eslintConfigPrettier,
+]
---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+export default {
+  trailingComma: 'es5',
+  semi: false,
+  singleQuote: true,
+  printWidth: 160,
+  tabWidth: 2,
+  useTabs: false,
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  singleAttributePerLine: false,
+  endOfLine: 'auto',
+}
