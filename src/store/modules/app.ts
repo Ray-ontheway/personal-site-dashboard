c670ca54064aa9a9ef0308362b81123133f1eafc
rayc <cr_fun@qq.com>
2024-11-08 01:26:22 +0800

    fix: 删除store/app.ts 中不需要的变量声明

--- src/store/modules/app.ts
+++ src/store/modules/app.ts
@@ -25,7 +25,6 @@ export const useAppStore = defineStore({
         }
         return true
       }
-      const curRoutes = routes.filter(item => doFilter(item))
       this.routes = routes.filter(item => doFilter(item))
     },
   },
